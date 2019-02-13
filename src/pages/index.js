89f31ee47ff2eb045d999003758e90d1cc49cd0a
khriztianmoreno <khriztianmoreno@gmail.com>
2019-02-13 17:10:13 -0500

    Code base

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,20 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO title="Home" keywords={['Azure Bootcamp 2019 - Medellin', 'Avanet', 'Community']} />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new Avanet site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <a href="//www.papercall.io/global-azure-2019">Call for Papers</a>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
